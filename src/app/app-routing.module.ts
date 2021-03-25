e2e480b9f368842f1e9ed6652ece7733180962b4
Sunkara Krishna Teja <ktsunkara@gmail.com>
2021-03-25 23:41:05 +0530

    ReCommitting the removed changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 import { TasksComponent } from './tasks/tasks.component';
 
@@ -9,10 +8,6 @@ const routes: Routes = [
   { path:'home', component: HomeComponent },
   {path:'tasks', component:TasksComponent}
 ];
-=======
-
-const routes: Routes = [];
->>>>>>> 721266002500144d4457cd729e3afb324dc19ab9
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
