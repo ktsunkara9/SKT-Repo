7939d7262cae72ec13b76f766266e645ef0c4f79
Sunkara Krishna Teja <ktsunkara@gmail.com>
2021-03-29 13:47:22 +0530

    Refactored the Tasks Module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks.component';
 import { RouterModule, Routes } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from '../app-routing.module';
 
 
 const routes : Routes = [
